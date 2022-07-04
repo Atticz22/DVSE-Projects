ce74b6f9abcaeed50f1ece78e15e68b09bf69e33
Atticz22 <attilacz22@gmail.com>
2022-07-04 22:14:05 +0300

    Added Projects

--- InstaJS/instagram-clone/src/components/post/image.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Image({ src, caption }) {
-  return (
-    <div className="post__img">
-      <img src={src} alt={caption} />
-    </div>
-  );
-}
